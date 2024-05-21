47c530d1fd2be4e5e82fb57164af862be039dfde
rubelmollik86 <rubelmollik86@gmail.com>
2024-05-22 02:58:38 +0600

    design product card without functionality

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import Banner from "../Banner/Banner"
-import Discount from "../Discount/Discount"
+import React from "react";
+import Banner from "../Banner/Banner";
+import Discount from "../Discount/Discount";
+import ProductCard from "../ProductCard/productCard";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner />
-            <Discount/>
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+      <Discount />
+      <ProductCard />
+    </div>
+  );
 };
 
 export default Home;
